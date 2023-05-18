04cd98259fd5ba7493179caa5c45c5ea6d296516
michael harris <alphasni@gmail.com>
2023-05-18 08:05:39 -0500

    added error handeling of JWT

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,12 @@ class ApplicationController < ActionController::API
   private 
   def authenticate_request
     header = request.headers["Authorization"]
-    header = header.split("").last if header
-    decoded = jwt_decode(header)
-    @current_user = User.find(decoded[:user_id])
+    header = header.split(" ").last if header
+    begin
+      decoded = jwt_decode(header)
+      @current_user = User.find(decoded[:user_id])
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
   end
 end
